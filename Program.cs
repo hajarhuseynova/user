c4089c258f5fad6518291ebe8448333b1d016a9e
hajar298 <huseynova_hecer@mail.ru>
2023-03-06 20:31:21 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Please,enter the username:");
+    string UserName = Console.ReadLine();
+    Console.WriteLine("Please,enter the user's password");
+    string Password = Console.ReadLine();
+
+    User User = new User(UserName, Password);
+
+    if (User.UserName != null && User.Password != null)
+    {
+        Console.WriteLine("You can access your profile");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("You can not access your profile,because you have a problem with your username or password");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,88 @@
+﻿class User
+{
+    private string _userName;
+    private string _password;
+
+ public string UserName
+    {
+        get
+        { return _userName; }
+        set
+        {
+            if (!value.Contains(" "))
+            {
+                _userName = value;
+            }
+        }
+    }
+    
+    public string Password
+    {
+        get
+        { return _password; }
+        set
+        {
+            if (CheckPassword(value))
+            {
+                _password = value;
+            }
+           
+        }
+    }
+    public User (string UserName, string Password)
+    {
+        if (CheckPassword(Password))
+        {
+
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+    }
+
+    public bool CheckPassword(string value)
+    {
+        if (!(value.Length >= 8))
+        {
+            return false;
+        }
+        if (value.Contains(" "))
+        {
+            return false;
+        }
+        if (true)
+        {
+            int count = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                string digits = i.ToString();
+                if (value.Contains(digits))
+                {
+                    count = 1;
+                }
+            }
+            if (count == 0)
+            {
+                return false;
+            }
+        }
+        if (true)
+        {
+            int count = 0;
+            for (int i = 65; i <= 90; i++)
+            {
+                char c = (char)i;
+                string letters = c.ToString();
+                if (value.Contains(letters))
+                {
+                    count = 1;
+                }
+            }
+            if (count == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
